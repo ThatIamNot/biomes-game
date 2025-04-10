7ee4dae66ec8bc30ce1f430df9176984d35a177d
ThatIamNot <34526617+ThatIamNot@users.noreply.github.com>
2025-04-10 22:22:45 +0200

    Update auth_manager.ts

--- src/client/game/context_managers/auth_manager.ts
+++ src/client/game/context_managers/auth_manager.ts
@@ -313,10 +313,74 @@ function prefetchAudioAssets(resources: ClientResources) {
 export async function loadAudioManager(loader: RegistryLoader<ClientContext>) {
   return new AudioManager(await loader.get("resources"));
 }
+export class BiomesUser {
+  constructor(
+    public readonly id: BiomesId,
+    public readonly createMs: number | undefined,
+    private specialRoles: ReadonlySet<SpecialRoles>
+  ) {}
+
+  hasSpecialRole(...requiredRoles: SpecialRoles[]) {
+    return evaluateRole(this.specialRoles, ...requiredRoles);
+  }
+
+  updateSpecialRoles(newRoles: ReadonlySet<SpecialRoles>) {
+    this.specialRoles = newRoles;
+  }
+}
+
+export class AuthManager {
+  constructor(public readonly currentUser: BiomesUser) {}
+
+  private static async fetchUserProfile(userId: BiomesId): Promise<BiomesUser> {
+    if (!userId) {
+      return new BiomesUser(INVALID_BIOMES_ID, undefined, new Set());
+    }
+    const profile: SelfProfileResponse = await asyncBackoffOnAllErrors(
+      async () => {
+        try {
+          return await jsonFetch<SelfProfileResponse>(
+            "/api/social/self_profile"
+          );
+        } catch (error) {
+          log.error("Error fetching self profile, retrying", { error });
+          throw error;
+        }
+      },
+      {
+        baseMs: 1000,
+        exponent: 1.25,
+        maxMs: 10000,
+      }
+    );
+    ok(userId === profile.user.id, "User ID mismatch");
+    return new BiomesUser(
+      profile.user.id,
+      profile.user.createMs,
+      new Set(profile.roles)
+    );
+  }
+
+  static async bootstrap(userId: BiomesId): Promise<AuthManager> {
+    return new AuthManager(await this.fetchUserProfile(userId));
+  }
+
+  static logout() {
+    fireAndForget(
+      logout().then(() => {
+        setTimeout(() => {
+          location.href = "/";
+        }, 100);
+      })
+    );
+  }
+}
 
 export async function loadAuthManager<C extends EarlyClientContext>(
   loader: RegistryLoader<C>
 ) {
   return AuthManager.bootstrap(await loader.get("userId"));
+}
+
 
 }
